9f22d329a4bb50a91cb32536c761132f5ed2c59d
amathenge <ann.mathenge@starrepublic1.onmicrosoft.com>
2022-07-13 12:03:10 +0200

    disclaimer

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -4,8 +4,9 @@ const Home = () => {
     return (
         <div className='home'>
             <h1>
-                Fun with <code>mix-blend-mode</code>!
+                JavaScript-free fun* with <code>mix-blend-mode</code>!
             </h1>
+            <p>* Cool, but just because you can doesn't mean you should</p>
         </div>
     )
 }
